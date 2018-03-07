b04783f2364e5efde6fe0c16fe0a01c507aa6a75
mjbertken <michael@bertken.com>
2018-03-07 16:02:41 -0600

    Update social_widgets.js

--- html/bertken_custom/social_widgets/social_widgets.js
+++ html/bertken_custom/social_widgets/social_widgets.js
@@ -6,7 +6,7 @@ function build_social_widget(context){
   var theTemplate = Handlebars.compile(theTemplateScript);
   
   // Add the compiled html to the page
-  var target_parent = '#social-row';
+  var target_parent = context.target_parent;
   var exists = $(target_parent).find('#'+template_id+'_'+context.container_id).length;
   if(exists>0){context['isnew']=false}else{context['isnew'] = true};
   var theCompiledHtml = theTemplate(context);
@@ -14,4 +14,27 @@ function build_social_widget(context){
   if(exists>0){$(target_parent).find('#'+template_id+'_'+context.container_id).html(theCompiledHtml)}
   if(exists==0){$(target_parent).append(theCompiledHtml)}
   $('#'+template_id+'_'+context.container_id).slideDown();
+}
+
+function generate_test_social_widget(){
+  var arr = ['facebook','twitter','instagram','linkedin','bbb','google'];
+  for(var a=0;a<arr.length;a++){
+    var platform = arr[a];
+    
+    var followers = 1230;
+    var followers_chg = 123;
+    var rating = 4.5;
+    var data = {
+      "target_parent":'#social-row',
+      "container_id":platform,
+      "platform":platform,
+      "label_1_i_class":"fa fa-thumbs-up",
+      "value_1":followers,
+      "label_2_i_class":"fa fa-rss",
+      "value_2":followers,
+      "followers_chg":'+'+followers_chg,
+      "rating":rating
+    }
+    build_social_widget(data);
+  }
 }
