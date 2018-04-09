d5ed9f08674f616220c6d7ed0f31ba4dbb68339b
mjbertken <michael@bertken.com>
2018-04-09 13:21:12 -0500

    Update dashboard6-data.js

--- html/fixed-width-light/dist/js/dashboard6-data.js
+++ html/fixed-width-light/dist/js/dashboard6-data.js
@@ -6,7 +6,7 @@
 $(window).load(function(){
 	window.setTimeout(function(){
 		$.toast({
-			heading: 'Test',
+			heading: '1Test',
 			text: 'Use the predefined ones, or specify a custom position object.',
 			position: 'bottom-left',
 			loaderBg:'#f8b32d',
