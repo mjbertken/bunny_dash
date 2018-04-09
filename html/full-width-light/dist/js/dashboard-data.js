6218a0e7c33aa5289ac9d312243f4a6be837e0a6
mjbertken <michael@bertken.com>
2018-04-09 13:22:05 -0500

    Update dashboard-data.js

--- html/full-width-light/dist/js/dashboard-data.js
+++ html/full-width-light/dist/js/dashboard-data.js
@@ -14,19 +14,19 @@ $(document).ready(function(){
 /*****Ready function end*****/
 
 /*****Load function start*****/
-$(window).load(function(){
-	window.setTimeout(function(){
-		$.toast({
-			heading: '2Test',
-			text: 'Use the predefined ones, or specify a custom position object.',
-			position: 'bottom-left',
-			loaderBg:'#e58b25',
-			icon: 'success',
-			hideAfter: 3500, 
-			stack: 6
-		});
-	}, 3000);
-});
+// $(window).load(function(){
+// 	window.setTimeout(function(){
+// 		$.toast({
+// 			heading: 'Test',
+// 			text: 'Use the predefined ones, or specify a custom position object.',
+// 			position: 'bottom-left',
+// 			loaderBg:'#e58b25',
+// 			icon: 'success',
+// 			hideAfter: 3500, 
+// 			stack: 6
+// 		});
+// 	}, 3000);
+// });
 /*****Load function* end*****/
 
 /*****E-Charts function start*****/
